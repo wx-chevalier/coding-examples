e6a637774d68bc432093da5d80e890e2a9dbbbcf
wxyyxc1992 <384924552@qq.com>
2018-03-25 23:30:12 +0800

    Update

---
+++ web/README.md
@@ -0,0 +1,6 @@
+# Web Coding Snippets
+
+# Index: 索引
+
+* WebWorker
+  * [sw-cache](): 通用缓存的 Service Worker
--- web/v8/hidden_classes.js
+++ web/v8/hidden_classes.js
@@ -1,13 +1,13 @@
 var PROPERTIES = 10000000;
 var o = {};
 
-var start = +new Date;
+var start = +new Date();
 
 for (var i = 0; i < PROPERTIES; i++) {
   o[i] = i;
 }
 
-console.log(+new Date - start);
+console.log(+new Date() - start);
 
 var PROPERTIES = 10000000;
 
@@ -19,32 +19,30 @@ function O(size) {
 
 var o = new O(PROPERTIES);
 
-var start = +new Date;
+var start = +new Date();
 
 for (var i = 0; i < PROPERTIES; i++) {
   o[i] = i;
 }
 
-console.log(+new Date - start);
+console.log(+new Date() - start);
 
 var PROPERTIES = 10000000;
 
 class OClass {
-
-    constructor(size){
-        for (var i = 0; i < size; i++) {
-            this[i] = null;
-        }
+  constructor(size) {
+    for (var i = 0; i < size; i++) {
+      this[i] = null;
     }
-
+  }
 }
 
 var o = new OClass(PROPERTIES);
 
-var start = +new Date;
+var start = +new Date();
 
 for (var i = 0; i < PROPERTIES; i++) {
   o[i] = i;
 }
 
-console.log(+new Date - start);
+console.log(+new Date() - start);
