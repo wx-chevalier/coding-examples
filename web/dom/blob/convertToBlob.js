348d874237a5b7092288ce0f63ae7f7398ece037
wxyyxc1992 <384924552@qq.com>
2018-07-25 20:49:46 +0800

    feat: update

--- web/dom/blob/convertToBlob.js
+++ web/dom/blob/convertToBlob.js
@@ -31,11 +31,13 @@ function b64toBlob(b64Data, contentType, sliceSize) {
 function dataURLtoBlob(dataURL) {
   // Decode the dataURL
   const binary = atob(dataURL.split(',')[1]);
+
   // Create 8-bit unsigned array
   const array = [];
   for (let i = 0; i < binary.length; i++) {
     array.push(binary.charCodeAt(i));
   }
+
   // Return our Blob object
   return new Blob([new Uint8Array(array)], { type: 'image/png' });
 }
